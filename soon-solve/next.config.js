68bfad8070bb5f0c3c540cfac26579dce4d9915e
ZaiKuku <willzau24531@gmail.com>
2023-08-12 21:16:11 +0800

    create next app

---
+++ soon-solve/next.config.js
@@ -0,0 +1,13 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = withPWA(nextConfig)
