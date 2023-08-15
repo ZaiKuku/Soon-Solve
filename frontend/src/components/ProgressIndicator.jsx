d7e9d9cd0df651539f32da3515bcb071f4eedd5b
Powen <owentseng1014@gmai.com>
2023-08-15 15:39:37 +0800

    add dynamic countdown timer

---
+++ frontend/src/components/ProgressIndicator.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+function ProgressIndicator({ createdAt, deadline }) {
+  // Convert to Date objects
+  const createdAtDate = new Date(createdAt);
+  const currentDateTime = new Date();
+  const deadlineDate = new Date(deadline);
+
+  // Calculate durations
+  const totalDuration = deadlineDate - createdAtDate;
+  const elapsedDuration = currentDateTime - createdAtDate;
+
+  // Calculate percentage progress
+  const progressPercentage = 100 - (elapsedDuration / totalDuration) * 100;
+
+  return (
+    <CircularProgress
+      variant="determinate"
+      value={progressPercentage}
+      style={{
+        color: "rgba(177, 94, 108, 1)",
+        width: "160px",
+        height: "160px",
+      }}
+      thickness={2}
+    />
+  );
+}
+
+export default ProgressIndicator;
