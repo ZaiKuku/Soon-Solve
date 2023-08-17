fd9fe596ddf0b4b3598e62416bbc19268eee418b
Powen <owentseng1014@gmai.com>
2023-08-17 15:24:24 +0800

    complete notification page

--- frontend/src/pages/notifications/index.jsx
+++ frontend/src/pages/notifications/index.jsx
@@ -1,14 +1,14 @@
-import Applicant from "@/components/Applicant";
+import Notification from "@/components/notification";
 import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
-export default function ApplicantsPage() {
+export default function NotificationsPage() {
   return (
     <main className="w-full flex flex-col gap-2 items-center pt-[80px]">
       <Header />
       <div className="w-[90%] flex flex-col gap-2 justify-center">
-        <Applicant />
+        <Notification />
       </div>
       <button
         style={{
--- frontend/src/pages/task/[id].jsx
+++ frontend/src/pages/task/[id].jsx
@@ -6,6 +6,7 @@ import NavBar from "@/components/NavBar";
 import styles from "../../styles/SingleTaskPage.module.scss";
 import Header from "@/components/Header";
 import useTaskDetails from "@/hooks/useTaskDetails";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function SingleTask() {
   const router = useRouter();
@@ -24,61 +25,19 @@ function SingleTask() {
         <Header />
         {/* <Task taskData={task} /> */}
         <Task />
-        <div className={styles.backContainer}>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="40"
-              viewBox="0 0 30 40"
-              fill="none"
-            >
-              <g filter="url(#filter0_d_2_405)">
-                <path
-                  d="M19.9333 0L4 15.9333L19.9333 31.8667L25.9083 25.8917L15.95 15.9333L25.9083 5.975L19.9333 0Z"
-                  fill="black"
-                />
-              </g>
-              <defs>
-                <filter
-                  id="filter0_d_2_405"
-                  x="0"
-                  y="0"
-                  width="29.9083"
-                  height="39.8667"
-                  filterUnits="userSpaceOnUse"
-                  colorInterpolationFilters="sRGB"
-                >
-                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                    result="hardAlpha"
-                  />
-                  <feOffset dy="4" />
-                  <feGaussianBlur stdDeviation="2" />
-                  <feComposite in2="hardAlpha" operator="out" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in2="BackgroundImageFix"
-                    result="effect1_dropShadow_2_405"
-                  />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow_2_405"
-                    result="shape"
-                  />
-                </filter>
-              </defs>
-            </svg>
-          </button>
-        </div>
+        <button
+          style={{
+            position: "absolute",
+            bottom: "100px",
+            left: "20px",
+          }}
+          onClick={() => {
+            // Handle click event here
+          }}
+        >
+          <ArrowBackIcon style={{ fontSize: "40px" }} />
+        </button>
+
         <NavBar />
       </div>
     </SWRConfig>
